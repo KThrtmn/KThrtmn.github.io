252df59efbeca3c2edd89b417d9c1afbb4f3d51e
KThrtmn <har16021@byui.edu>
2020-10-15 17:26:10 -0500

    js push

---
+++ wdd230/lesson05/pancakes.js
@@ -0,0 +1,5 @@
+function PANCAKES(){
+if(day==thursday){
+    document.writeln("<p>PANCAKES IN THE PARK! 9:00am Saturday at the city park pavilion.</p>");
+document.getElementById('pan').innerHTML=PANCAKES
+}

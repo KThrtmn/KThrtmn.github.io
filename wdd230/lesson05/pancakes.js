f6846a27319bdf63f8f3c1c160025bebc95e429a
KThrtmn <har16021@byui.edu>
2020-10-21 11:20:32 -0500

    pancakes works

--- wdd230/lesson05/pancakes.js
+++ wdd230/lesson05/pancakes.js
@@ -1,7 +1,7 @@
 var today = new Date();
 var day1 = today.getDay();
 
-if (day1 == 2) {
+if (day1 == 5) {
 
     document.getElementById('pancakes')
         .innerHTML = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";

f7be4873dca4df1202fefd8b9b335499dfa99808
KThrtmn <har16021@byui.edu>
2020-10-24 15:27:27 -0500

    lesson6

---
+++ wdd230/lesson06/pancakes6.js
@@ -0,0 +1,9 @@
+var today = new Date();
+var day1 = today.getDay();
+
+if (day1 == 5) {
+
+    document.getElementById('pancakes')
+        .innerHTML = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";
+
+}

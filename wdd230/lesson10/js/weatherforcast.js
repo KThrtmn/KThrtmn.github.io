8baa79a5c657ac50234a259c364e7e783526eed0
KThrtmn <har16021@byui.edu>
2020-11-30 15:18:19 -0600

    lesson 10 progress

--- wdd230/lesson10/js/weatherforcast.js
+++ wdd230/lesson10/js/weatherforcast.js
@@ -20,16 +20,16 @@ fetch(apiURL)
         chill.textContent = getWindChill(jsObject.wind.speed, jsObject.main.temp);
     });
 
-    function getWindChill(wind, curtem) {
-        if (curtem <= 50 && wind > 3.0) {
-            var chill = (35.75 + (0.6215 * curtem)) -
-                (35.75 * Math.pow(wind, 0.16)) +
-                (0.42575 * curtem * Math.pow(wind, 0.16));
-            chill = Math.round(chill);
-            return chill;
-        } else
-            return 'N/A';
-    }
+function getWindChill(wind, curtem) {
+    if (curtem <= 50 && wind > 3.0) {
+        var chill = (35.75 + (0.6215 * curtem)) -
+            (35.75 * Math.pow(wind, 0.16)) +
+            (0.42575 * curtem * Math.pow(wind, 0.16));
+        chill = Math.round(chill);
+        return chill;
+    } else
+        return 'N/A';
+}
 
 
 
@@ -39,12 +39,12 @@ fetch(apiURL)
     .then(response => response.json())
     .then((jsObject) => {
         //console.log(jsObject);
-        
+
         const weatherIcon = document.querySelector("#icon");
         const forecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
         console.log(forecast);
         var daysOfTheWeek = ["SUN", "MON", "TUE", "WED", "THURS", "FRI", "SAT"];
-       
+
 
         for (let i = 0; i <= forecast.length - 1; i++) {
             var temp = forecast[i].main.temp;
@@ -52,10 +52,10 @@ fetch(apiURL)
             console.log(date1);
             var weekDay = daysOfTheWeek[date1.getDay()];
             console.log(weekDay);
-            
+
             weekDay = document.querySelector("#weekDay" + (i + 1).toString());
             weekDay.innerHTML = daysOfTheWeek[date1.getDay()];
-    
+
             temp = document.querySelector("#currentTemp" + (i + 1).toString());
             temp.innerHTML = forecast[i].main.temp;
 
@@ -64,7 +64,7 @@ fetch(apiURL)
 
             getIcon(data, i);
         }
-        
+
     });
 
 
@@ -75,41 +75,34 @@ fetch(apiURL)
 
 
 
-function getIcon(data, index){
-
-    fetch(apiURL)
-    .then(response => response.json())
-    .then((jsObject) => {
-        console.log(jsObject);
-
-        //selecting html id, naming constent
-        //const curtemp = document.querySelector("#current-temp");
-        const iconSource = document.querySelector("#imagesrc");
-        const weatherIcon = document.querySelector("#icon" + (index + 1).toString());
-
-        //pathing to infomation from constent
-       // curtemp.innerHTML = jsObject.main.temp;
-
-        const imagesrc = 'https://openweathermap.org/img/w/' + data + '.png';
-        console.log(imagesrc);
-    
-        weatherIcon.setAttribute('src', imagesrc);
-
-        //in a loop
-        index.toString() + "#icon" + (index + 1).toString();
-
-        console.log("#icon" + (index + 1).toString());
+function getIcon(data, index) {
 
 
-       // document.getElementById('imagesrc').textContent = imagesrc;
-        //document.getElementById('icon').setAttribute('src', imagesrc);
-        //document.getElementById('icon').setAttribute('alt', desc);
-    });
+
+    //selecting html id, naming constent
+    //const curtemp = document.querySelector("#current-temp");
+    const iconSource = document.querySelector("#imagesrc");
+    const weatherIcon = document.querySelector("#icon" + (index + 1).toString());
+
+    //pathing to infomation from constent
+    // curtemp.innerHTML = jsObject.main.temp;
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + data + '.png';
+    console.log(imagesrc);
+
+    weatherIcon.setAttribute('src', imagesrc);
+
+    //in a loop
+    index.toString() + "#icon" + (index + 1).toString();
+
+    console.log("#icon" + (index + 1).toString());
+
+
+    // document.getElementById('imagesrc').textContent = imagesrc;
+    //document.getElementById('icon').setAttribute('src', imagesrc);
+    //document.getElementById('icon').setAttribute('alt', desc);
+
 }
 
 
 //getIcon();
-
-
-
-    
